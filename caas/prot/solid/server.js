b2a287d7a52d757940cd6180540083742437c0b7
franciscodcosta <franciscodcosta@yahoo.com.br>
2016-09-15 17:23:53 -0300

    configurando node

--- caas/prot/solid/server.js
+++ caas/prot/solid/server.js
@@ -2,4 +2,4 @@ var http = require ('http');
 http.createServer(function(req, res){
 	res.writeHead(200, {'Content-Type': 'text/plain'});
 	res.end('servidor node de teste.\n');
-}).listen(3000, '127.0.0.1');
+}).listen(80, '127.0.0.1');
